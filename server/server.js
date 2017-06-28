b871708f52155c49a10ff17801e4f788a1298477
Mariusz Rosa <m.rosa@ebros.pl>
2017-06-28 16:56:35 +0200

    socket conne i diss

--- server/server.js
+++ server/server.js
@@ -1,15 +1,32 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const publicPath = path.join(__dirname, '/../public');
+const socketIO = require('socket.io');
 
 console.log(__dirname + '/../public');
 console.log(publicPath);
 
 var app = express();
 const port = process.env.PORT || 3000;
+// var server = http.createServer((req, res) => {
+
+// });
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // app.use(bodyParser.json());
 app.use(express.static(publicPath))
+
+io.on('connection', (socket) => {
+    console.log('new user');
+
+    socket.on('disconnect', (socket) => {
+        console.log('disss');
+    })
+});
+
+
 
 app.use((req, res, next) => {
     var now = new Date().toString();
@@ -35,7 +52,7 @@ app.use((req, res, next) => {
 // })
 
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Started on port ' + port);
 });
 
