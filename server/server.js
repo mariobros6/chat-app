1422475d02fc328fda84b658569178c72bb4e469
Mariusz Rosa <m.rosa@ebros.pl>
2017-06-28 22:40:22 +0200

    messages

--- server/server.js
+++ server/server.js
@@ -33,7 +33,12 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log(message);
-        socket.emit('newMessage', message);
+        // socket.emit('newMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime(),
+        })
     })
 });
 
